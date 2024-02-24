1a5651ebab8bacb7a38e28beca76957ad76b2286
Gess1t <tintamare91@gmail.com>
2024-02-24 01:41:28 +0100

    Initial commit

---
+++ src/CustomLED.cs
@@ -0,0 +1,38 @@
+﻿using OpenTabletDriver.Plugin;
+using OpenTabletDriver.Plugin.Attributes;
+
+namespace OTD.CustomLED;
+
+public class CustomLED : ITool
+{
+    #region Initialization
+
+    public bool Initialize()
+    {
+        _ = Task.Run(InitializeCore);
+
+        return true;
+    }
+
+    private Task InitializeCore()
+    {
+        
+    }
+
+    #endregion
+
+    #region Properties
+
+
+
+    #endregion
+
+    #region Disposal
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+}
